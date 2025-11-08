94f4a5db9cb21268d406db064bc5d5971972c154
Camilo Andres Conrado Benavides <camiloconrado1@gmail.com>
2025-11-08 11:41:43 -0500

    Commit 1: Inicialización del proyecto con Express + estructura base

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API funcionando correctamente");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
